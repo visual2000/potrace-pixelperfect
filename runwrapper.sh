cd17cfb77c7dc0ae73e08343c9789805bbf59003
Paul <423357+toothbrush@users.noreply.github.com>
2020-04-19 12:06:41 +1000

    Smidgen faster

--- runwrapper.sh
+++ runwrapper.sh
@@ -7,8 +7,9 @@ export PATH=${HOME}/bin:/usr/local/bin:${PATH}
 
 # Otherwise we get unbound ZSH_VERSION :(
 # TODO remove, this is temporary until asfd is updated.  Fix is in master.
-set +u
-. /usr/local/opt/asdf/asdf.sh
-set -u
+# set +u
+# . /usr/local/opt/asdf/asdf.sh
+# set -u
 
+export PATH="${HOME}/.asdf/shims:${PATH}"
 "${DIR}/potrace-pp.py" $@
